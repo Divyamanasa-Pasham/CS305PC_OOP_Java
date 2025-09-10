a9796ff4bbe8db756a5f528ad59168e2aa2ee6f4
Divyamanasa-Pasham <divyamanasa871@gmail.com>
2025-09-10 12:33:45 +0530

    Add files via upload

---
+++ Lab2p1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Lab2p1{
+	public static void main(String...args){
+	Scanner s = new Scanner(System.in);
+ 	int a = s.nextInt();
+ 	s.nextLine();
+ 	String op = s.nextLine();
+ 	switch(op){
+ 		case "++":
+ 			System.out.println("post:"+(a++));
+ 			System.out.println("pre:"+(++a));
+ 			break;
+ 		case "--":
+ 			System.out.println("post:"+(a--));
+ 			System.out.println("pre:"+(--a));
+ 			break;
+ 		case "+":
+ 			a = +a;
+ 			System.out.println("Unary +: "+(a));
+ 			break;
+ 		case "-":
+ 			a = -a;
+ 			System.out.println("Unary +:" +(a));
+ 			break;
+ 		case "~":
+ 			a = ~a;
+ 			System.out.println("Unary+:" +(~a)); 
+ 			break;		
+ 		default:
+ 			System.out.println("enter the wrong op");
+ 			System.out.println("the out :"+ (a++ + ++a));
+ 		 }
+	}
+}
