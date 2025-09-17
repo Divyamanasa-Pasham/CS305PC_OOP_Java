426b74d3f1c5a98538083a6979247522a66d2340
Divyamanasa-Pasham <divyamanasa871@gmail.com>
2025-09-17 12:59:00 +0530

    Add files via upload

---
+++ lab3p41.java
@@ -0,0 +1,12 @@
+class lab3p51{
+	String name;
+	public void demonstate(){
+		System.out.println("I am a method in Lab3p51);
+		}
+	public void setName(String s){
+		this.name = s;
+		}
+	public String getname(){
+		return this.name;
+		}
+}
